a21db0232be4d4417bc07ec6ee963a0feda33ca8
jwd-katten <jwd.katten@gmail.com>
2023-06-15 17:14:33 -0700

    shell basics

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include<stdio.h>
+
+/**
+ * main - prints "Programming is like building a ....
+ * Return 0 if exited 
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return(0);
+}
