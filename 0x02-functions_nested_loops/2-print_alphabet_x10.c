dd9c25bec4cd44f6db854a7b31ae35aea1588326
jwd-katten <jwd.katten@gmail.com>
2023-06-20 11:16:42 -0700

    complete task 2-print alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,23 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet_x10 -  print the alphabet 10 times
  *
  * Return: Always 0 (Success)
- */
-int main(void)
-{
-	return (0);
-}
+*/
 
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10  ; i++)
+	{
+		int ch;
+
+		for (ch = 'a' ; ch <= 'z' ; ch++)
+		{
+			_putchar(ch);
+		}
+	_putchar('\n');
+	}
+}
