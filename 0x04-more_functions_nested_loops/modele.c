954b0ac80da1e96f0098b42fc61ba93078632087
jwd-katten <jwd.katten@gmail.com>
2023-06-22 06:16:19 -0700

    comlplete first task 0-isupper

---
+++ 0x04-more_functions_nested_loops/modele.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * name_function - description
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: return value
+ */
+
+int name_function(int a, int b, int c)
+{
+	int largest;
+
+
+	return (largest);
+}
