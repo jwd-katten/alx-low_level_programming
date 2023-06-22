5f2ba800ab9a6decd3f7471ded24c13a64b752c6
jwd-katten <jwd.katten@gmail.com>
2023-06-22 06:47:11 -0700

    complete task 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include"main.h"
+
+/**
+ * mul - function that multiples two integers
+ * @a : first integer
+ * @b : socond integer
+ * Return: result of multiplication
+*/
+
+int mul(int a, int b)
+{
+		return (a * b);
+}
