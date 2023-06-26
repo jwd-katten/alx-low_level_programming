52f9a3cc18d647b805a4af46a0b5cce5711643e5
jwd-katten <jwd.katten@gmail.com>
2023-06-26 06:11:10 -0700

    correction

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,21 @@
 
 void print_rev(char *s)
 {
-	int i;
-	int len;
+	int i, j, len;
 
-	len = _strlen(s);
+	i = 0;
 
-	for (i = len ; i >= 0 ; i--)
+	while (s[i] != '\0')
 	{
-		_putchar(s[i]);
+		i++;
 	}
+
+	len = i;
+
+	for (j = len - 1 ; j >= 0 ; j--)
+	{
+		_putchar(s[j]);
+	}
+
 	_putchar('\n');
 }
