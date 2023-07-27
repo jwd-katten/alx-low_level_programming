854ad8fd01111a27ac5778039e4d03625a3385da
jwd-katten <jwd.katten@gmail.com>
2023-07-27 04:18:39 -0700

    correction 2

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,9 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
+#ifndef _LISTS_H
+#define _LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -14,13 +18,12 @@ typedef struct list_s
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-} list_t;
+}list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void before_main() __attribute__((constructor));
 
-#endif
+#endif /* _LISTS_H */
