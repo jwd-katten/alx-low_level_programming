9d4185a7d65b0fe4d6bc2797dd700fcd6304f5f4
jwd-katten <jwd.katten@gmail.com>
2023-07-27 04:24:17 -0700

    correction and task 2

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,38 +1,19 @@
 #include "lists.h"
-#include <string.h>
 
 /**
- * _strlen - returns the length of a string
- * @s: string to be measured
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: pointer to the head of the list
  *
- * Return: the length of the string
+ * Return: the number of nodes
  */
-int _strlen(const char *s)
+size_t list_len(const list_t *h)
 {
-	int i = 0;
+	size_t i = 0;
 
-	while (s[i])
+	while (h)
+	{
+		h = h->next;
 		i++;
+	}
 	return (i);
-}
-
-/**
- * add_node - adds a new node at the beginning of a list_t list
- * @head: pointer to the head of the list
- * @str: string to be duplicated
- *
- * Return: the address of the new element, or NULL if it failed
- */
-list_t *add_node(list_t **head, const char *str)
-{
-	list_t *new;
-
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-	new->str = strdup(str);
-	new->len = _strlen(str);
-	new->next = *head;
-	*head = new;
-	return (new);
 }
