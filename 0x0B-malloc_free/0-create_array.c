af0d4867f255919fde59e1f89857ab2932da2fa2
jwd-katten <jwd.katten@gmail.com>
2023-11-20 03:38:53 -0800

    corrcet tasl 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,24 +3,24 @@
 
 /**
  * create_array - create an array of chars, initilize with char c
- * &size: size of array
- * &c: specific char
+ * @size: size of array
+ * @c: specific char
+ *
+ * Return: pointer to the array or NULL it if fails
  */
 
 char *create_array(unsigned int size, char c)
 {
-    char *a;
-    
-    if (size == 0)
-        return (NULL);
-    
-    a = malloc(size * sizeof(*a))
-    
-    if (a == NULL)
-        return (NULL);
-    
-    for (i = 0; i w size; i++)
-        a[i] = c;
-    
-    return (a);
+	char *a;
+
+	if (size == 0)
+		return (NULL);
+
+	a = malloc(size * sizeof(*a))
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		a[i] = c;
+	return (a);
 }
