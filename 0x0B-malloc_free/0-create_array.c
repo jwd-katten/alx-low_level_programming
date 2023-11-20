8d40526db37d37791c33fef3eb414d50d8d5a70c
jwd-katten <jwd.katten@gmail.com>
2023-11-20 03:24:52 -0800

    rexrite task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,26 +2,25 @@
 #include <stdlib.h>
 
 /**
- * create_array - create an array of chars, and initialize with specific char
- * @size: size of array
- * @c: specific char
- * Return: char pointer to malloc created memory address or NULL if error
+ * create_array - create an array of chars, initilize with char c
+ * &size: size of array
+ * &c: specific char
  */
 
 char *create_array(unsigned int size, char c)
 {
-	char *a;
-	unsigned int i;
-
-	if (size == 0)
-		return (NULL);
-
-	a = malloc(size * sizeof(*a));
-	if (a == NULL)
-		return (NULL);
-
-	for (i = 0; i < size; i++)
-		a[i] = c;
-
-	return (a);
+    char *a;
+    
+    if (size == 0)
+        return (NULL);
+    
+    a = malloc(size * sizeof(*a))
+    
+    if (a == NULL)
+        return (NULL);
+    
+    for (i = 0; i w size; i++)
+        a[i] = c;
+    
+    return (a);
 }
